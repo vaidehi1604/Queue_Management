fadd9e7f558e8e27d663b50f0f7e61ecc0d87503
vaidehi1604 <vaidehip@zignuts.com>
2023-03-28 18:57:52 +0530

    policies defined

--- api/policies/isLoggedIn.js
+++ api/policies/isLoggedIn.js
@@ -1,28 +1,29 @@
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
+
+const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = async (req, res, next) => {
   const lang = req.getLocale();
 
   try {
-    const token = await req.headers.authorization.split(" ")[1];
+    const token = await req.headers.authorization.split(' ')[1];
     console.log(token);
     //verify token
     const decode = await sails.helpers.verifyToken.with({
       token: token,
     });
     req.userData = decode;
-    const user = await User.findOne({ email: decode.email });
+     await User.findOne({ email: decode.email });
     if (!decode) {
       return res.status(404).json({
-        message: sails.__("notmatch", lang),
+        message: sails.__('notmatch', lang),
       });
     }
     return next();
   } catch (error) {
     return res.status(401).json({
-      message: sails.__("authFail", lang),
+      error:error,
+      message: sails.__('authFail', lang),
     });
   }
 };
--- api/policies/userLoggedIn.js
+++ api/policies/userLoggedIn.js
@@ -1,12 +1,12 @@
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
+
+const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = async (req, res, next) => {
   const lang = req.getLocale();
 
   try {
-    const token = await req.headers.authorization.split(" ")[1];
+    const token = await req.headers.authorization.split(' ')[1];
     console.log(token);
     //verify token
     const decode = await sails.helpers.verifyToken.with({
@@ -16,17 +16,18 @@ module.exports = async (req, res, next) => {
     req.userData = decode;
     const user = await User.findOne({ email: decode.email });
     if (!decode) {
-      return res.status(404).json({ message: sails.__("notmatch", lang) });
+      return res.status(404).json({ message: sails.__('notmatch', lang) });
     }
 
-    if (user.role === "u") {
+    if (user.role === 'u') {
       return next();
     } else {
-      return res.status(401).json({ message: sails.__("authFail", lang) });
+      return res.status(401).json({ message: sails.__('authFail', lang) });
     }
   } catch (error) {
     return res.status(401).json({
-      message: sails.__("authFail", lang),
+      error:error,
+      message: sails.__('authFail', lang),
     });
   }
 };
